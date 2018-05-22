6cb9d221e255bcf305cdea3a32d805c05aa368e4
mikejohnslade <39481054+mikejohnslade@users.noreply.github.com>
2018-05-22 08:47:53 +0100

    Added place a bet activity

---
+++ app/src/main/java/com/example/jackn/shake_on_it/activity_place_a_bet.java
@@ -0,0 +1,13 @@
+package com.example.jackn.shake_on_it;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_place_a_bet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_a_bet);
+    }
+}
--- app/src/main/java/com/example/jackn/shake_on_it/home.java
+++ app/src/main/java/com/example/jackn/shake_on_it/home.java
@@ -14,9 +14,13 @@ public class home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
     }
 
-
     public void goToHelp (View view){
         Intent intent = new Intent (home.this, help.class);
+        startActivity(intent);
+    }
+
+    public void goToBet (View view){
+        Intent intent = new Intent (home.this, activity_place_a_bet.class);
         startActivity(intent);
     }
 }
